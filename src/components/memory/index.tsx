f202e06e8a3b704712a55146f146e4cdaffefa24
Eduard Metzler <eduard.metzler@mail.ru>
2023-08-11 23:48:47 +0200

    added items

--- src/components/memory/index.tsx
+++ src/components/memory/index.tsx
@@ -9,72 +9,7 @@ interface Items {
 }
 
 export const Memory: React.FC<any> = ({}) => {
-  const [items, setItems] = useState<Items[]>([
-    // {
-    //   className: "fa-solid fa-users-rectangle fa-2xl",
-    //   show: false,
-    //   clear: false,
-    // },
-    // { className: "fa-solid fa-vault fa-2xl", show: false, clear: false },
-    // {
-    //   className: "fa-solid fa-vial-circle-check fa-2xl",
-    //   show: false,
-    //   clear: false,
-    // },
-    // {
-    //   className: "fa-solid fa-wand-magic-sparkles fa-2xl",
-    //   show: false,
-    //   clear: false,
-    // },
-    // { className: "fa-solid fa-code-compare fa-2xl", show: false, clear: false },
-    // { className: "fa-solid fa-xmarks-lines fa-2xl", show: false, clear: false },
-    // { className: "fa-solid fa-wheat-awn fa-2xl", show: false, clear: false },
-    // {
-    //   className: "fa-solid fa-turkish-lira-sign fa-2xl",
-    //   show: false,
-    //   clear: false,
-    // },
-    // { className: "fa-solid fa-truck-field fa-2xl", show: false, clear: false },
-    // { className: "fa-solid fa-trowel fa-2xl", show: false, clear: false },
-    // { className: "fa-solid fa-tree-city fa-2xl", show: false, clear: false },
-    // {
-    //   className: "fa-solid fa-tower-observation fa-2xl",
-    //   show: false,
-    //   clear: false,
-    // },
-    // {
-    //   className: "fa-solid fa-users-rectangle fa-2xl",
-    //   show: false,
-    //   clear: false,
-    // },
-    // { className: "fa-solid fa-vault fa-2xl", show: false, clear: false },
-    // {
-    //   className: "fa-solid fa-vial-circle-check fa-2xl",
-    //   show: false,
-    //   clear: false,
-    // },
-    // {
-    //   className: "fa-solid fa-wand-magic-sparkles fa-2xl",
-    //   show: false,
-    //   clear: false,
-    // },
-    // { className: "fa-solid fa-code-compare fa-2xl", show: false, clear: false },
-    // { className: "fa-solid fa-xmarks-lines fa-2xl", show: false, clear: false },
-    // { className: "fa-solid fa-wheat-awn fa-2xl", show: false, clear: false },
-    // {
-    //   className: "fa-solid fa-turkish-lira-sign fa-2xl",
-    //   show: false,
-    //   clear: false,
-    // },
-    // { className: "fa-solid fa-truck-field fa-2xl", show: false, clear: false },
-    // { className: "fa-solid fa-trowel fa-2xl", show: false, clear: false },
-    // { className: "fa-solid fa-tree-city fa-2xl", show: false, clear: false },
-    // {
-    //   className: "fa-solid fa-tower-observation fa-2xl",
-    //   show: false,
-    //   clear: false,
-    // },
-  ]);
+  const [items, setItems] = useState<Items[]>([]);
 
   const [selected, setSelected] = useState(["", ""]);
 
@@ -92,7 +27,41 @@ export const Memory: React.FC<any> = ({}) => {
     "fa-solid fa-trowel fa-2xl",
     "fa-solid fa-tree-city fa-2xl",
     "fa-solid fa-tower-observation fa-2xl",
+    "fa-solid fa-tower-cell fa-2xl",
+    "fa-solid fa-toilets-portable fa-2xl",
+    "fa-solid fa-timeline fa-2xl",
+    "fa-solid fa-tents fa-2xl",
+    "fa-solid fa-sun-plant-wilt fa-2xl",
+    "fa-solid fa-stapler fa-2xl",
+    "fa-solid fa-staff-snake fa-2xl",
+    "fa-solid fa-shrimp fa-2xl",
+    "fa-solid fa-shop-lock fa-2xl",
+    "fa-solid fa-shield-heart fa-2xl",
+    "fa-solid fa-sheet-plastic fa-2xl",
+    "fa-solid fa-section fa-2xl",
+    "fa-solid fa-sailboat fa-2xl",
+    "fa-solid fa-sack-xmark fa-2xl",
+    "fa-solid fa-road-bridge fa-2xl",
+    "fa-solid fa-road-barrier fa-2xl",
+    "fa-solid fa-plate-wheat fa-2xl",
+    "fa-solid fa-plant-wilt fa-2xl",
+    "fa-solid fa-plane-up fa-2xl",
+    "fa-solid fa-person-walking-luggage fa-2xl",
+    "fa-solid fa-person-through-window fa-2xl",
+    "fa-solid fa-person-drowning fa-2xl",
+    "fa-solid fa-people-line fa-2xl",
+    "fa-solid fa-oil-well fa-2xl",
+    "fa-solid fa-mountain-city fa-2xl",
+    "fa-solid fa-mosquito fa-2xl",
+    "fa-solid fa-money-bills fa-2xl",
+    "fa-solid fa-mobile-retro fa-2xl",
+    "fa-solid fa-location-pin-lock fa-2xl",
+    "fa-solid fa-landmark-flag fa-2xl",
+    "fa-solid fa-kitchen-set fa-2xl",
+    "fa-solid fa-jar-wheat fa-2xl",
+    "fa-solid fa-jar fa-2xl",
   ];
+  console.log(icons.length);
 
   const newGame = () => {
     const newItems = [];
@@ -161,7 +130,7 @@ export const Memory: React.FC<any> = ({}) => {
       );
     } else if (selected[0] && selected[1] && selected[0] !== selected[1]) {
       setItems((prev) =>
-        prev.map((item, i) => (item.show ? { ...item, show: false } : item))
+        prev.map((item) => (item.show ? { ...item, show: false } : item))
       );
       setItems((prev) =>
         prev.map((item, i) =>
@@ -201,6 +170,15 @@ export const Memory: React.FC<any> = ({}) => {
         })}
       </div>
       <button onClick={newGame}>Noch Mal</button>
+      {/* {items.map((item) => {
+        return (
+          <div className={styles.memoryItem}>
+            <div className={styles.memoryItemValueShow}>
+              <i className={item.className}></i>
+            </div>
+          </div>
+        );
+      })} */}
     </>
   );
 };
